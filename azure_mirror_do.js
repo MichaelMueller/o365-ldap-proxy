31af7f3b176be18c3297ae97abe144f987066fc9
mueller <mueller@mbits.info>
2017-08-22 14:32:15 +0200

    server working

--- azure_mirror_do.js
+++ azure_mirror_do.js
@@ -1,165 +1,66 @@
 var auth = require('./auth');
 var graph = require('./graph');
 var config = require('./config');
-var encode = require( 'hashcode' ).hashCode;
+var fs = require('fs'); 
 
 var azure_mirror_do = {};
-azure_mirror_do.getDomainObject = function() {
 
-  var domainDc = config.baseDn.substring(0, config.baseDn.indexOf(","));
-  var domainObj = {
-    
-      dn: config.baseDn,
-      attributes: {
-        objectClass: ['dcObject', 'organization', "top", "OpenLDAProotDSE"],
-        dc: domainDc,
-        name: domainDc,
-        entrydn: config.baseDn,
-        o: config.orgName,        
-        entryuuid: "46364f2a-186f-1037-8a89-23e38af4fd13",
-        "subschemaSubentry": ["cn=subschema"],
-        structuralObjectClass: "organization",
-        "distinguishedName": config.baseDn,
-        rootDomainNamingContext: config.baseDn,
-        namingContexts: [config.baseDn]
-      }
-  };  
-  return domainObj;
-}
-
-azure_mirror_do.getAllGroupObject = function(allGroupId, memberUids, uniqueMembers) {
-  
-  // add group for all
-  var groupObj = {
-      dn: "cn=" + config.allGroupName + "," + config.baseDn,
-      attributes: {
-        objectclass: ['groupOfUniqueNames', 'posixgroup', 'top', 'sambaGroupMapping'],
-        cn: config.allGroupName,
-        gidNumber: allGroupId,
-        description: "Group "+config.allGroupName,              
-        entryuuid: "4637e56a-186f-1037-8a8a-23e38af4fd13",
-	"sambaSID": "S-1-5-21-"+allGroupId,
-        memberuid: memberUids,
-        uniqueMember: uniqueMembers,                         
-        structuralObjectClass: "posixGroup",
-        subschemaSubentry: "cn=Subschema"
-      }
-  };
-  return groupObj;
-}
-
-azure_mirror_do.getUserObject = function(graphObj, allGroupId) {
-  var uid = (graphObj.userPrincipalName);
-  if(config.removeDomainFromCn)
-    uid = uid.replace("@"+config.azureDomain, '');
-  var myCn = graphObj.displayName;
-  var myDn = "uid=" + uid + "," + config.baseDn;
-  var hash = Math.abs(encode().value( graphObj.id )); 
-  
-  var ldapObj = {
-    dn: myDn,
-    attributes: {
-      objectclass: ['inetorgperson', 'posixaccount', 'top', 'shadowAccount', 'person', 'sambaSamAccount'],
-      cn: graphObj.displayName,
-      entryuuid: graphObj.id,
-      gidNumber: allGroupId,      
-      givenName: graphObj.givenName,
-      sn: graphObj.surname,
-      displayName: graphObj.displayName,
-      mail: graphObj.mail,
-      "uid": uid,
-      "sambaSID": "S-1-5-21-"+hash,
-      uidNumber: hash,
-      userPassword: hash,
-      homeDirectory: "/home/"+uid,
-      structuralObjectClass: "inetOrgPerson",
-      shadowExpire: -1,
-      shadowFlag: 0,
-      shadowLastChange: 0,
-      shadowMax: 99999,
-      shadowMin: 0,
-      shadowWarning: 7
-    }
-  };
-  
-  return ldapObj;
-}
-
-azure_mirror_do.run = function (user, pass, cb)
+azure_mirror_do.run = function ()
 {    
   // Get an access token for the app.
-  auth.getAccessToken().then(function (token) {
+  auth.getAccessToken().then(function (token) 
+  {
     // Get all of the users in the tenant.
-    graph.getUsers(token)
-      .then(function (users) {
-        var data = [];      
+    graph.getUsers(token).then(function (users) 
+    {
+      //console.dir(users);
+      
+      var graphUsers = [];  
+      for (var i = 0, len = users.length; i < len; i++) 
+      {
+        var graphObj = users[i];
+        if(config.removeDomainFromCn)
+          graphObj["userPrincipalName"] = graphObj["userPrincipalName"].replace("@"+config.azureDomain, "");
         
-        var domainObj = azure_mirror_do.getDomainObject();
-        data.push(domainObj);
-          var groupid = Math.abs(encode().value( config.allGroupName )); 
-        
-        var uniqueMembers = [];    
-        var memberUids = [];    
-        
-        for (var i = 0, len = users.length; i < len; i++) 
+        if(config.excludeUsers)
         {
-          var graphObj = users[i];
-          
-          //if( config.skipUsersNotInAzureDomain && graphObj.userPrincipalName.indexOf(config.azureDomain) == -1 )
-            //continue;
-          if(config.excludeUsers)
+          var skip = false;
+          for (var k = 0, len2 = config.excludeUsers.length; k < len2; k++) 
           {
-            var skip = false;
-            for (var k = 0, len2 = config.excludeUsers.length; k < len2; k++) 
+            if( graphObj.userPrincipalName.search(config.excludeUsers[k]) > -1 )
             {
-              if( graphObj.userPrincipalName.search(config.excludeUsers[k]) > -1 )
-              {
-                skip = true;
-                break;
-              }
+              skip = true;
+              break;
             }
-            
-            if(skip)
-            {            
-              console.log("Skipping "+ graphObj.userPrincipalName+" due to exclude rules" )
-              continue;          
-            }
-            
           }
           
-         
-          var ldapObj = azure_mirror_do.getUserObject(graphObj, groupid);
-          data.push(ldapObj);
-          
-          uniqueMembers.push(ldapObj.dn);          
-          memberUids.push(ldapObj.attributes.uid);
+          if(skip)
+          {            
+            console.log("Skipping "+ graphObj.userPrincipalName+" due to exclude rules" )
+            continue;          
+          }          
         }
-        
-        
-        data.push(azure_mirror_do.getAllGroupObject(groupid, memberUids, uniqueMembers));
-        
-        for (var i = 0, len = config.includeUser.length; i < len; i++) 
-        {        
-          data.push(config.includeUser[i]);
-        }      
-        
-        const fs = require('fs');
-        const content = JSON.stringify(data, null, 2);
+        graphUsers.push(graphObj);                 
+      }
+      var db = JSON.parse(fs.readFileSync(config.dataFile, 'utf8'));
+      db = config.updateDatabase(graphUsers, db);
+      const content = JSON.stringify(db, null, 2);
 
-        fs.writeFile(config.dataFile, content, 'utf8', function (err) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log("Mirror file was saved!");
-        }); 
+      fs.writeFile(config.dataFile, content, 'utf8', function (err) {
+          if (err) {
+              console.log(err);
+          }
+          console.log("New Database file saved!");
+      });   
         
-      }, function (error) {
-        console.error('>>> Error getting users: ' + error);
-      });
-  }, function (error) {
-    console.error('>>> Error getting access token: ' + error);
+    }, function (error) 
+    {
+        console.log('>>> Error getting users: ' + error);
+    });
+  }, function (error) 
+  {
+    console.log('>>> Error getting access token: ' + error);
   });
-
 }; 
  
  module.exports = azure_mirror_do;
--- server.js
+++ server.js
@@ -10,118 +10,291 @@ console.log = function () {
 var ldap = require('ldapjs');
 var config = require('./config');
 var user_auth = require('./user_auth');
-var fs = require('fs');
-var azure_mirror_do = require('./azure_mirror_do');
+var fs = require('fs'); 
+var schedule = require('node-schedule'); 
+var nthash = require('smbhash').nthash;
 
-if (!fs.existsSync(config.dataFile)) 
+///--- Globals
+var SUFFIX = '';
+var db = {};
+var schemaDb = {};
+var lastModTime = 0;
+var server = ldap.createServer();
+
+function saveDb()
 {
-  console.log("Initial mirroring necessary. Running now");   
-  azure_mirror_do.run();
+  const content = JSON.stringify(db, null, 2);
+
+  fs.writeFile(config.dataFile, content, 'utf8', function (err) {
+      if (err) {
+          return console.log(err);
+      }
+      console.log("Database file saved!");
+  });   
 }
-if( config.mirrorScheduleCronExpression )
-{  
-  console.log("Configuring scheduler using cron rule "+config.mirrorScheduleCronExpression);   
-  var schedule = require('node-schedule'); 
+function loadDbIfNecessary()
+{
+  var stats = fs.statSync(config.dataFile);
+  var mtime = stats.mtime;
+    console.log("Checking if database is outdated. lastModTime: "+lastModTime.toString()+", mtime: "+mtime.toString());
+  if(mtime > lastModTime)
+  {
+    console.log("Database outdated. Rereading from "+config.dataFile);
+    lastModTime = mtime;
+    db = JSON.parse(fs.readFileSync(config.dataFile, 'utf8'));
+  }
+}
+
+///--- START
+// read datafile initially
+loadDbIfNecessary();
+console.log("Reading Schema Data file "+config.schemaDataFile);
+schemaDb = JSON.parse(fs.readFileSync(config.schemaDataFile, 'utf8'));
+
+// scheduler
+if( !config.mirrorScheduleCronExpression )
+    console.log('!!Scheduler Cron Expression missing!!');
+else
+{
+  console.log("Configuring scheduler using cron rule "+config.mirrorScheduleCronExpression);  
   var j = schedule.scheduleJob(config.mirrorScheduleCronExpression, function(){
     console.log("************** Starting scheduled mirroring");
     azure_mirror_do.run();
   });
 }
-else
-{  
-  console.log("Scheduler is disabled");   
-}
+  
+// ADMIN BIND
+/*
+server.bind(config.adminDn, function(req, res, next) {
+  adminAtts = db[config.adminDn];
+  if (req.dn.toString().toLowerCase().replace(/ /g, '') !== config.adminCn || req.credentials !== adminAtts.userPassword)
+    return next(new ldap.InvalidCredentialsError());
 
-var server = ldap.createServer();
+  res.end();
+  return next();
+});
+*/
 
-
-server.bind(config.baseDn, function(req, res, next) 
-{
-  var username = req.dn.toString().replace(/ /g, '');
-  // anonymous bind
-  if(username == "" && config.enableAnonymousLogin == true)
-    return next();
+server.bind(SUFFIX, function(req, res, next) {
+  // USING AZURE LOGIN HERE
+  var theDn = req.dn.toString().toLowerCase().replace(/ /g, '');
   
   // dn bind
-  if( (username).indexOf(config.baseDn.toLowerCase()) !== -1)
-  {  
-    username = username.replace(',' + config.baseDn.toLowerCase(), '');  
-    username = username.substr(username.indexOf("=")+1);
-  }
+  theDn = theDn.replace(config.userRdn+"=",'');
+  theDn = theDn.replace(","+config.usersGroupDn,'');
+  username = theDn;
   if(config.removeDomainFromCn == true)
-    username = username+"@"+config.azureDomain;  
+    username = username+"@"+config.azureDomain;
   
-  var pass = req.credentials;
-  
+  var pass = req.credentials;  
   user_auth.checkUserNameAndPass(username, pass, function(err)
   {
     if(err)
     {      
-      console.log(username+" failed login");            
-
+      console.log(username+": Failed login");
       return next(new ldap.InvalidCredentialsError());    
+    }
+    loadDbIfNecessary();
+    theDn = config.userRdn+"="+theDn+","+config.usersGroupDn;
+    var userAtts = db[theDn];
+    if(userAtts && userAtts.hasOwnProperty("sambaNTPassword"))
+    {    
+      var userNtHash = nthash(pass);
+      if(userAtts["sambaNTPassword"] != userNtHash)
+      {
+        console.log("Saving NT password hash for user "+theDn);
+        userAtts["sambaNTPassword"] = userNtHash;    
+        db[theDn] = userAtts;
+        saveDb();
+      }
     }
     
     console.log(username+" successfully authenticated");
     res.end();
     return next();
   });
-
 });
 
-server.search("", function(req, res, next) {
-  var baseObj = req.dn.toString().replace(/ /g, ''); 
-  if(req.hasOwnProperty("attributes"))
-    attStr = req.attributes.toString();
-  console.log('Search started. Base object: ' + baseObj + '. Scope: ' + req.scope + '. Filter: ' + req.filter.toString() + ' Attributes: ' + attStr); 
-  
-  var tmp_data = JSON.parse(fs.readFileSync(config.dataFile, 'utf8'));  
-  for (var i = 0; i < tmp_data.length; i++) 
-  {
-    if( req.scope == "one" && tmp_data[i].dn == config.baseDn ) 
-      continue;
-    if (req.filter.matches(tmp_data[i].attributes)) 
-    {
-      res.send(tmp_data[i]);
+/*
+server.compare(SUFFIX, authorize, function(req, res, next) {
+  var theDn = req.dn.toString().toLowerCase().replace(/ /g, '');
+  if (!db[theDn])
+    return next(new ldap.NoSuchObjectError(theDn));
+
+  if (!db[theDn][req.attribute])
+    return next(new ldap.NoSuchAttributeError(req.attribute));
+
+  var matches = false;
+  var vals = db[theDn][req.attribute];
+  for (var i = 0; i < vals.length; i++) {
+    if (vals[i] === req.value) {
+      matches = true;
+      break;
     }
   }
-  res.end();
+
+  res.end(matches);
+  return next();
 });
 
-server.search(config.baseDn, function(req, res, next) {
-  var baseObj = req.dn.toString().replace(/ /g, ''); 
+server.add(SUFFIX, authorize, function(req, res, next) {
+  var theDn = req.dn.toString().toLowerCase().replace(/ /g, '');
+
+  if (db[theDn])
+    return next(new ldap.EntryAlreadyExistsError(theDn));
+
+  db[theDn] = req.toObject().attributes;
+  saveDb();
+  res.end();
+  return next();
+});
+
+server.del(SUFFIX, authorize, function(req, res, next) {
+  var theDn = req.dn.toString().toLowerCase().replace(/ /g, '');
+  if (!db[theDn])
+    return next(new ldap.NoSuchObjectError(theDn));
+
+  delete db[theDn];
+  saveDb();
+
+  res.end();
+  return next();
+});
+
+server.modify(SUFFIX, authorize, function(req, res, next) {
+  var theDn = req.dn.toString().toLowerCase().replace(/ /g, '');
+  if (!req.changes.length)
+    return next(new ldap.ProtocolError('changes required'));
+  if (!db[theDn])
+    return next(new ldap.NoSuchObjectError(theDn));
+
+  var entry = db[theDn];
+
+  for (var i = 0; i < req.changes.length; i++) {
+    mod = req.changes[i].modification;
+    switch (req.changes[i].operation) {
+    case 'replace':
+      if (!entry[mod.type])
+        return next(new ldap.NoSuchAttributeError(mod.type));
+
+      if (!mod.vals || !mod.vals.length) {
+        delete entry[mod.type];
+      } else {
+        entry[mod.type] = mod.vals;
+      }
+
+      break;
+
+    case 'add':
+      if (!entry[mod.type]) {
+        entry[mod.type] = mod.vals;
+      } else {
+        mod.vals.forEach(function(v) {
+          if (entry[mod.type].indexOf(v) === -1)
+            entry[mod.type].push(v);
+        });
+      }
+
+      break;
+
+    case 'delete':
+      if (!entry[mod.type])
+        return next(new ldap.NoSuchAttributeError(mod.type));
+
+      delete entry[mod.type];
+
+      break;
+    }
+  }
+
+  saveDb();
+  res.end();
+  return next();
+});
+ */
+ 
+//server.search(SUFFIX, authorize, function(req, res, next) {
+server.search(SUFFIX, function(req, res, next) 
+{
+  loadDbIfNecessary();
+  var theDn = req.dn.toString().toLowerCase().replace(/ /g, '');
+  var attStr = "";
   if(req.hasOwnProperty("attributes"))
     attStr = req.attributes.toString();
-  console.log('Search started. Base object: ' + baseObj + '. Scope: ' + req.scope + '. Filter: ' + req.filter.toString() + ' Attributes: ' + attStr);  
-  
-  var tmp_data = JSON.parse(fs.readFileSync(config.dataFile, 'utf8'));
-  if(baseObj == config.baseDn)
+  console.log('Search started. Base object: ' + theDn + '. Scope: ' + req.scope + '. Filter: ' + req.filter.toString() + ' Attributes: ' + attStr); 
+
+  //
+  if(schemaDb.hasOwnProperty(theDn))
   {
-    for (var i = 0; i < tmp_data.length; i++) 
-    {
-      if( req.scope == "one" && tmp_data[i].dn == config.baseDn ) 
-        continue;
-      if (req.filter.matches(tmp_data[i].attributes)) 
-      {
-        res.send(tmp_data[i]);
-      }
-    }    
+    res.send({
+      dn: theDn,
+      attributes: schemaDb[theDn]
+    });
+    res.end();
+    return next();    
   }
-  else
+  else if(theDn=="")
   {
-    for (var i = 0; i < tmp_data.length; i++) 
-    {      
-      if( req.scope == "base" && tmp_data[i].dn == baseObj ) 
-      {
-        res.send(tmp_data[i]);
-        break;
-      }      
-    }    
+    theDn = config.baseDn;
   }
-    
+
+  if (!db[theDn])    
+    return next(new ldap.NoSuchObjectError(theDn));
+
+  var scopeCheck;
+
+  switch (req.scope) {
+  case 'base':
+    if (req.filter.matches(db[theDn])) {
+      console.log("sending "+theDn);
+      res.send({
+        dn: theDn,
+        attributes: db[theDn]
+      });
+    }
+
+    res.end();
+    return next();
+
+  case 'one':
+    scopeCheck = function(k) {
+      if (req.dn.equals(k))
+        return false;
+
+      var parent = ldap.parseDN(k).parent();
+      return (parent ? parent.equals(req.dn) : false);
+    };
+    break;
+
+  case 'sub':
+    scopeCheck = function(k) {
+      return (req.dn.equals(k) || req.dn.parentOf(k));
+    };
+
+    break;
+  }
+
+  Object.keys(db).forEach(function(key) {
+    if (!scopeCheck(key))
+      return;
+
+    if (req.filter.matches(db[key])) {
+      console.log("sending "+key);
+      res.send({
+        dn: key,
+        attributes: db[key]
+      });
+    }
+  });
+
   res.end();
+  return next();
 });
 
-server.listen(config.port, function() {
-  console.log('/etc/passwd LDAP server up at: %s', server.url);
+
+
+///--- Fire it up
+
+server.listen(389, function() {
+  console.log('LDAP server up at: %s', server.url);
 });
